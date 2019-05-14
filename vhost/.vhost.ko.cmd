190caed5e87fdbbd905e320c30de1053816d8521
jjudrgn <wndyd9557@gmail.com>
2019-05-14 20:07:48 +0900

    vhost file

---
+++ vhost/.vhost.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/jy-os/lab/test3/vhost/vhost.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/jy-os/lab/test3/vhost/vhost.ko /home/jy-os/lab/test3/vhost/vhost.o /home/jy-os/lab/test3/vhost/vhost.mod.o ;  true
